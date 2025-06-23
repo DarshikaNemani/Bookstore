51dc0c866902ce13165c44cde999d0908be25441
DarshikaNemani <dk5673@srmist.edu.in>
2025-06-24 02:43:15 +0530

    Add: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginPageComponent } from './pages/login/login.component';
+import { WishlistComponent } from './pages/wishlist/wishlist.component';
 import { authGuard } from './guards/auth.guard';
 import { DevComponent } from './pages/dev/dev.component';
 import { BookHeroComponent } from './components/book-hero/book-hero.component';
 import { OrderComponent } from './pages/order/order.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
 import { AddressFormComponent } from './components/address-form/address-form.component';
+import { MyOrdersComponent } from './pages/my-orders/my-orders.component';
 
 export const routes: Routes = [
   {
@@ -19,8 +22,18 @@ export const routes: Routes = [
     canActivate: [authGuard]
   },
   {
+    path: 'wishlist',
+    component: WishlistComponent,
+    canActivate: [authGuard]
+  },
+  {
     path: 'order',
     component: OrderComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'order/success',
+    component: OrderSuccessComponent,
     canActivate: [authGuard]
   },
   {
@@ -37,12 +50,11 @@ export const routes: Routes = [
     component: BookHeroComponent
   },
   {
-    path: 'dev',
-    component: AddressFormComponent
+    path: 'myorders',
+    component: MyOrdersComponent
   },
   {
     path: '**',
     redirectTo: '/home'
   },
-
 ];
