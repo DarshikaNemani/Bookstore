a63121de80bbf76720d03e9d9cae0b45726992e2
DarshikaNemani <dk5673@srmist.edu.in>
2025-06-24 02:42:33 +0530

    Ref: integrate with order page

--- src/app/components/order-success/order-success.component.ts
+++ src/app/components/order-success/order-success.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-order-success',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './order-success.component.html',
   styleUrl: './order-success.component.css'
 })
 export class OrderSuccessComponent {
+  orderId = '#' + Math.floor(Math.random() * 1000000); // Generate random order ID
 
+  constructor(private router: Router) {}
+
+  continueShopping(): void {
+    this.router.navigate(['/home']);
+  }
 }
