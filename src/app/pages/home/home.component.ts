45b0edf31f1ae6495c20d7e9c3d6741cd43004ce
DarshikaNemani <dk5673@srmist.edu.in>
2025-06-27 01:14:44 +0530

    Add: sort by

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,6 +5,7 @@ import { BookCardComponent } from '../../components/book-card/book-card.componen
 import { PaginationComponent } from '../../components/pagination/pagination.component';
 import { ProductService } from '../../services/product.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Book } from '../../services/models';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
@@ -16,6 +17,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     BookCardComponent,
     PaginationComponent,
     CommonModule,
+    FormsModule,
     MatProgressSpinnerModule,
   ],
   templateUrl: './home.component.html',
@@ -29,6 +31,7 @@ export class HomeComponent implements OnInit {
   itemsPerPage: number = 12;
   isLoading: boolean = false;
   searchTerm: string = '';
+  selectedSort: string = 'default';
 
   constructor(private productService: ProductService) {}
 
@@ -44,6 +47,7 @@ export class HomeComponent implements OnInit {
         if (response.success && response.result) {
           this.allBooks = response.result;
           this.filteredBooks = this.allBooks;
+          this.applySorting();
           this.updateDisplayedBooks();
           console.log('Books loaded: ', this.allBooks);
         } else {
@@ -68,7 +72,36 @@ export class HomeComponent implements OnInit {
     } else {
       this.filteredBooks = this.allBooks;
     }
+    this.applySorting();
     this.updateDisplayedBooks();
+  }
+
+  onSortChanged(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    this.selectedSort = target.value;
+    this.currentPage = 1;
+    this.applySorting();
+    this.updateDisplayedBooks();
+  }
+
+  applySorting(): void {
+    if (this.selectedSort === 'price-low-to-high') {
+      this.filteredBooks.sort((a, b) => {
+        const priceA = a.discountPrice > 0 ? a.discountPrice : a.price;
+        const priceB = b.discountPrice > 0 ? b.discountPrice : b.price;
+        return priceA - priceB;
+      });
+    } else if (this.selectedSort === 'price-high-to-low') {
+      this.filteredBooks.sort((a, b) => {
+        const priceA = a.discountPrice > 0 ? a.discountPrice : a.price;
+        const priceB = b.discountPrice > 0 ? b.discountPrice : b.price;
+        return priceB - priceA;
+      });
+    } else if (this.selectedSort === 'new-arrivals') {
+      this.filteredBooks.sort((a, b) => {
+        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+      });
+    }
   }
 
   updateDisplayedBooks(): void {
